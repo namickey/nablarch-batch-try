7d0a09d555c4fa7713282da1a94cade1185740b2
namickey <teethdai@gmail.com>
2025-11-24 22:36:08 +0900

    update

--- src/main/java/com/example/domain/SampleDomainBean.java
+++ src/main/java/com/example/domain/SampleDomainBean.java
@@ -10,27 +10,24 @@ public class SampleDomainBean {
 
     /** 公共団体コード */
     @Length(min = 5, max = 5)
-    @SystemChar(charsetDef = "数字")
+    @SystemChar(charsetDef = "半角数字")
     private String localGovernmentCode;
 
     /** 旧郵便番号 */
     @Length(min = 5, max = 5)
-    @SystemChar(charsetDef = "数字とスペース")
     private String oldZipCode;
 
     /** 郵便番号 */
     @Length(max = 7, min = 7)
-    @SystemChar(charsetDef = "数字")
+    @SystemChar(charsetDef = "半角数字")
     private String zipCode;
 
     /** 都道府県名 */
     @Length(max = 10)
-    @SystemChar(charsetDef = "地名漢字")
     private String prefecture;
 
     /** 市区町村名漢字 */
     @Length(max = 50)
-    @SystemChar(charsetDef = "地名漢字")
     private String city;
 
     /** 町域 */
@@ -39,21 +36,20 @@ public class SampleDomainBean {
 
     /** 都道府県名(カナ) */
     @Length(max = 10)
-    @SystemChar(charsetDef = "半角カタカナ")
+    @SystemChar(charsetDef = "半角カナ")
     private String prefectureKana;
 
     /** 市区町村名(カナ) */
     @Length(max = 50)
-    @SystemChar(charsetDef = "半角カタカナ")
+    @SystemChar(charsetDef = "半角カナ")
     private String cityKana;
 
     /** フラグ */
     @Length(min = 1, max = 1)
-    @SystemChar(charsetDef = "フラグ")
     private String flag;
 
     /** コード */
     @Length(min = 1, max = 1)
-    @SystemChar(charsetDef = "数字")
+    @SystemChar(charsetDef = "半角数字")
     private String code;
 }
